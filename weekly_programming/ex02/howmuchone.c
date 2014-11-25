ba8f2405b6cc7c6d59ca792b0548394f6ffa5532
liu <liu@liu-NV49C.(none)>
2014-11-25 18:08:59 +0800

    howmuchone

--- weekly_programming/ex02/howmuchone.c
+++ weekly_programming/ex02/howmuchone.c
@@ -1,3 +1,21 @@
+/*==========================================
+ Filename:
+      howmuchone.c
+
+ Copyright (C) 2014 Wistron
+ All rights reserved.
+
+ Description:
+      This file is used to calculate how much one in a binary number.
+
+ Author/Created Date:
+      Lumi-liu, Nov22,2014
+
+ Modification History:
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
