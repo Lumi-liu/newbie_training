85165eafd29a9f88cfa1f29e611ee2dd001c8fc4
Lumi-liu <1005229639@qq.com>
2014-12-11 16:15:22 +0800

    dev_test

--- linux_android_basic_training/linux_drivers_training/ex_04/dev_test.c
+++ linux_android_basic_training/linux_drivers_training/ex_04/dev_test.c
@@ -34,31 +34,31 @@ Note:
 #include <sys/stat.h>  
 #include <sys/ioctl.h>  
 
-#define SCULL_IOC_MAGIC  'k'   
-#define SCULL_IOCRESET    _IO(SCULL_IOC_MAGIC, 0)  
+#define GLOBALMEM_IOC_MAGIC  'k'   
+#define GLOBALMEM_IOCRESET    _IO(GLOBALMEM_IOC_MAGIC, 0)  
   
-#define SCULL_IOCSQUANTUM _IOW(SCULL_IOC_MAGIC,  1, int)  
-#define SCULL_IOCSQSET    _IOW(SCULL_IOC_MAGIC,  2, int)  
-#define SCULL_IOCTQUANTUM _IO(SCULL_IOC_MAGIC,   3)  
-#define SCULL_IOCTQSET    _IO(SCULL_IOC_MAGIC,   4)  
-#define SCULL_IOCGQUANTUM _IOR(SCULL_IOC_MAGIC,  5, int)  
-#define SCULL_IOCGQSET    _IOR(SCULL_IOC_MAGIC,  6, int)  
-#define SCULL_IOCQQUANTUM _IO(SCULL_IOC_MAGIC,   7)  
-#define SCULL_IOCQQSET    _IO(SCULL_IOC_MAGIC,   8)  
-#define SCULL_IOCXQUANTUM _IOWR(SCULL_IOC_MAGIC, 9, int)  
-#define SCULL_IOCXQSET    _IOWR(SCULL_IOC_MAGIC,10, int)  
-#define SCULL_IOCHQUANTUM _IO(SCULL_IOC_MAGIC,  11)  
-#define SCULL_IOCHQSET    _IO(SCULL_IOC_MAGIC,  12)  
-  
-  
-#define SCULL_P_IOCTSIZE _IO(SCULL_IOC_MAGIC,   13)  
-#define SCULL_P_IOCQSIZE _IO(SCULL_IOC_MAGIC,   14)  
+#define GLOBALMEM_IOCSQUANTUM _IOW(GLOBALMEM_IOC_MAGIC,  1, int)  
+#define GLOBALMEM_IOCSQSET    _IOW(GLOBALMEM_IOC_MAGIC,  2, int)  
+#define GLOBALMEM_IOCTQUANTUM _IO(GLOBALMEM_IOC_MAGIC,   3)  
+#define GLOBALMEM_IOCTQSET    _IO(GLOBALMEM_IOC_MAGIC,   4)  
+#define GLOBALMEM_IOCGQUANTUM _IOR(GLOBALMEM_IOC_MAGIC,  5, int)  
+#define GLOBALMEM_IOCGQSET    _IOR(GLOBALMEM_IOC_MAGIC,  6, int)  
+#define GLOBALMEM_IOCQQUANTUM _IO(GLOBALMEM_IOC_MAGIC,   7)  
+#define GLOBALMEM_IOCQQSET    _IO(GLOBALMEM_IOC_MAGIC,   8)  
+#define GLOBALMEM_IOCXQUANTUM _IOWR(GLOBALMEM_IOC_MAGIC, 9, int)  
+#define GLOBALMEM_IOCXQSET    _IOWR(GLOBALMEM_IOC_MAGIC,10, int)  
+#define GLOBALMEM_IOCHQUANTUM _IO(GLOBALMEM_IOC_MAGIC,  11)  
+#define GLOBALMEM_IOCHQSET    _IO(GLOBALMEM_IOC_MAGIC,  12)  
   
   
-#define SCULL_IOC_MAXNR 14 
+#define GLOBALMEM_P_IOCTSIZE _IO(GLOBALMEM_IOC_MAGIC,   13)  
+#define GLOBALMEM_P_IOCQSIZE _IO(GLOBALMEM_IOC_MAGIC,   14)  
+  
+  
+#define GLOBALMEM_IOC_MAXNR 14 
 
   
-#define SCULL_DEVICE "/dev/globalmem"  
+#define GLOBALMEM_DEVICE "/dev/globalmem"  
   
 int main(int argc, char *argv[])  
 {  
@@ -68,50 +68,50 @@ int main(int argc, char *argv[])
     int qset = 2000;  
     int qset_old = 0;  
       
-    fd = open(SCULL_DEVICE, O_RDWR);  
+    fd = open(GLOBALMEM_DEVICE, O_RDWR);  
     if(fd < 0)  
     {  
-        printf("open scull device error!\n");  
+        printf("open GLOBALMEM device error!\n");  
         return 0;  
     }  
   
-    printf("SCULL_IOCSQUANTUM: quantum = %d\n", quantum);  
-    ioctl(fd, SCULL_IOCSQUANTUM, &quantum);  
+    printf("GLOBALMEM_IOCSQUANTUM: quantum = %d\n", quantum);  
+    ioctl(fd, GLOBALMEM_IOCSQUANTUM, &quantum);  
     quantum -= 500;  
-    printf("SCULL_IOCTQUANTUM: quantum = %d\n", quantum);      
-    ioctl(fd, SCULL_IOCTQUANTUM, quantum);  
+    printf("GLOBALMEM_IOCTQUANTUM: quantum = %d\n", quantum);      
+    ioctl(fd, GLOBALMEM_IOCTQUANTUM, quantum);  
       
-    ioctl(fd, SCULL_IOCGQUANTUM, &quantum);  
-    printf("SCULL_IOCGQUANTUM: quantum = %d\n", quantum);      
-    quantum = ioctl(fd, SCULL_IOCQQUANTUM);  
-    printf("SCULL_IOCQQUANTUM: quantum = %d\n", quantum);      
+    ioctl(fd, GLOBALMEM_IOCGQUANTUM, &quantum);  
+    printf("GLOBALMEM_IOCGQUANTUM: quantum = %d\n", quantum);      
+    quantum = ioctl(fd, GLOBALMEM_IOCQQUANTUM);  
+    printf("GLOBALMEM_IOCQQUANTUM: quantum = %d\n", quantum);      
   
     quantum -= 500;  
-    quantum_old = ioctl(fd, SCULL_IOCHQUANTUM, quantum);  
-    printf("SCULL_IOCHQUANTUM: quantum = %d, quantum_old = %d\n", quantum, quantum_old);      
+    quantum_old = ioctl(fd, GLOBALMEM_IOCHQUANTUM, quantum);  
+    printf("GLOBALMEM_IOCHQUANTUM: quantum = %d, quantum_old = %d\n", quantum, quantum_old);      
     quantum -= 500;  
-    printf("SCULL_IOCXQUANTUM: quantum = %d\n", quantum);  
-    ioctl(fd, SCULL_IOCXQUANTUM, &quantum);  
-    printf("SCULL_IOCXQUANTUM: old quantum = %d\n", quantum);  
+    printf("GLOBALMEM_IOCXQUANTUM: quantum = %d\n", quantum);  
+    ioctl(fd, GLOBALMEM_IOCXQUANTUM, &quantum);  
+    printf("GLOBALMEM_IOCXQUANTUM: old quantum = %d\n", quantum);  
   
-    printf("SCULL_IOCSQSET: qset = %d\n", qset);  
-    ioctl(fd, SCULL_IOCSQSET, &qset);  
+    printf("GLOBALMEM_IOCSQSET: qset = %d\n", qset);  
+    ioctl(fd, GLOBALMEM_IOCSQSET, &qset);  
     qset += 500;  
-    printf("SCULL_IOCTQSET: qset = %d\n", qset);  
-    ioctl(fd, SCULL_IOCTQSET, qset);  
+    printf("GLOBALMEM_IOCTQSET: qset = %d\n", qset);  
+    ioctl(fd, GLOBALMEM_IOCTQSET, qset);  
   
-    ioctl(fd, SCULL_IOCGQSET, &qset);  
-    printf("SCULL_IOCGQSET: qset = %d\n", qset);  
-    qset = ioctl(fd, SCULL_IOCQQSET);  
-    printf("SCULL_IOCQQSET: qset = %d\n", qset);  
+    ioctl(fd, GLOBALMEM_IOCGQSET, &qset);  
+    printf("GLOBALMEM_IOCGQSET: qset = %d\n", qset);  
+    qset = ioctl(fd, GLOBALMEM_IOCQQSET);  
+    printf("GLOBALMEM_IOCQQSET: qset = %d\n", qset);  
   
     qset += 500;  
-    qset_old = ioctl(fd, SCULL_IOCHQSET, qset);  
-    printf("SCULL_IOCHQSET: qset = %d, qset_old = %d\n", qset, qset_old);      
+    qset_old = ioctl(fd, GLOBALMEM_IOCHQSET, qset);  
+    printf("GLOBALMEM_IOCHQSET: qset = %d, qset_old = %d\n", qset, qset_old);      
     qset += 500;  
-    printf("SCULL_IOCXQSET: qset = %d\n", qset);          
-    ioctl(fd, SCULL_IOCXQSET, &qset);  
-    printf("SCULL_IOCHQSET: old qset = %d\n", qset);  
+    printf("GLOBALMEM_IOCXQSET: qset = %d\n", qset);          
+    ioctl(fd, GLOBALMEM_IOCXQSET, &qset);  
+    printf("GLOBALMEM_IOCHQSET: old qset = %d\n", qset);  
   
     return 0;  
 }
